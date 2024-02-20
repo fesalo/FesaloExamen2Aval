af8f938ffa9003db3117ff86901d4dd98fc18d76
Felipe <fesalo@floridauniversitaria.es>
2024-02-20 09:27:11 +0000

    emenor done

--- emenor.sh
+++ emenor.sh
@@ -1,11 +1,16 @@
-
-    touch ciudades.txt
-    total=0
-    cont=0
-    while read ciudad mes ano consumo; do
-        if [ ! `grep $ciudad ciudades.txt` ];then
-            echo $ciudad >> ciudades.txt
-        fi
+touch cities.txt
+while read ciudad mes ano consumo ; do
+    if [ ! `grep $ciudad cities.txt` ];then
+        echo $ciudad >> cities.txt
+    fi
+done< consumos.txt
+while read ciudad mes ano consumo; do
+    if [ `grep $ciudad cities.txt` ];then
+            echo " `./cmedia.sh $ciudad`" $ciudad>> medias.txt 
+    fi
     done< consumos.txt
-
+    
+    sort medias.txt| head -n2 |tail -n1| awk '{print $2}'
+rm cities.txt
+rm medias.txt
 
